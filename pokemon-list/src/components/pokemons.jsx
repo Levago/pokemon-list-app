09d61a9fd93e6dcefa25b728873d63bcd90f1010
Jorge Luis Marmolejo santos <jorgel.marmolejo96@hotmail.com>
2022-07-02 21:48:45 -0500

    create usePokemon hook

--- pokemon-list/src/components/pokemons.jsx
+++ pokemon-list/src/components/pokemons.jsx
@@ -1,15 +1,25 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
+import usePokemon from '../hooks/usePokemon';
 
 const Pokemons = () => {
-
-    const [pokemons, setPokemons] = useState([]);
-
-    useEffect(() => {
-        const response = fetch('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0');
-        console.log(response);
-    }, [])
-
-    return <p>Lista de pokemones</p>
+    
+    const [actual, setActual] = useState('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0');
+    const { pokemons, loading, anterior, siguiente } = usePokemon(actual)
+    
+    return (
+        loading ?
+            <p>Crgando pokemones...</p>
+        :
+        <div>
+            <ul>        
+                {pokemons.map((pokemons, index) => {
+                    return <li key={index}>{pokemons.name}</li>
+                })}
+            </ul>
+            <button onClick={() => anterior !== null && setActual(anterior)}>Anteriores</button>
+            <button onClick={() => siguiente !== null && setActual(siguiente)}>Siguientes</button>
+        </div>
+    )
 }
 
 export default Pokemons;
